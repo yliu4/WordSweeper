71391a01252a5bc03b92b5fd4c768d6b1bef66d4
mtian0415 <tmwj1001@gmail.com>
2016-11-03 11:50:59 -0400

    update the comment for the choosePrivateController

--- MyWordSweeper/src/client/controller/ChoosePrivateController.java
+++ MyWordSweeper/src/client/controller/ChoosePrivateController.java
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-package client.controller;
-
-import client.view.Application;
-
-/**
- * The <code>ChoosePrivateController</code> class enables the editing of password field.
- * 
- * @author Team Pisces
- * @since  2016-11-01
- */
-public class ChoosePrivateController {
-	Application app;
-	
-	/**
-	 * ChoosePrivateController constructor
-	 *
-	 * @param app  	 initialize application
-	 */
-	public ChoosePrivateController(Application app) {
-		this.app = app;
-	}
-	
-	/**
-	 * set password's text field editable when choosing private mode
-	 */
-	public void process() {
-		app.getCreateGamePanel().getTextFieldPassword().setEditable(true);
-	}
-}
-=======
 package client.controller;
 
 import client.view.Application;
@@ -50,4 +19,4 @@ public class ChoosePrivateController {
 		app.getCreateGamePanel().getTextFieldPassword().setEditable(true);
 	}
 }
->>>>>>> fa9d3b560f674996e7fae66cc42e8847f0e48a1a
+
