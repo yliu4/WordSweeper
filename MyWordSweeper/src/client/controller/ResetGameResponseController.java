a2292aa8ed310d0a0903ebdf971fdd53d68e733b
yenchanghsieh <frank4754231@gmail.com>
2016-11-14 20:44:48 -0500

    Update findword lock reset response controller

--- MyWordSweeper/src/client/controller/ResetGameResponseController.java
+++ MyWordSweeper/src/client/controller/ResetGameResponseController.java
@@ -44,23 +44,29 @@ public class ResetGameResponseController extends ControllerChain {
 			return next.process(response);
 		}
 		
-		Node boardResponse = response.contents.getFirstChild();
-		NamedNodeMap map = boardResponse.getAttributes();
+		boolean success = response.success();
 		
-		String gameId = map.getNamedItem("gameId").getNodeValue();
+		if(success) {
 		
-		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		int height = d.height;
-		
-		//Show warning message
-		UIManager.put("OptionPane.buttonFont", 
-				new FontUIResource(new Font("Tahoma", Font.PLAIN, height/36)));
-		UIManager.put("OptionPane.messageFont", 
-				new FontUIResource(new Font("Times New Roman", Font.PLAIN, 2*height/45)));
-		String message = "The game" + gameId + "is ";
-		JOptionPane.showMessageDialog(app.getJoinGamePanel(), message, "Error!",
-        JOptionPane.ERROR_MESSAGE);
-		
+		} else {
+			String reason = response.reason();
+			Node boardResponse = response.contents.getFirstChild();
+			NamedNodeMap map = boardResponse.getAttributes();
+			
+			String gameId = map.getNamedItem("gameId").getNodeValue();
+			
+			Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+			int height = d.height;
+			
+			//Show warning message
+			UIManager.put("OptionPane.buttonFont", 
+					new FontUIResource(new Font("Tahoma", Font.PLAIN, height/36)));
+			UIManager.put("OptionPane.messageFont", 
+					new FontUIResource(new Font("Times New Roman", Font.PLAIN, 2*height/45)));
+			String message = "The game" + gameId + "can't be reset because of " + reason;
+			JOptionPane.showMessageDialog(app.getJoinGamePanel(), message, "Error!",
+	        JOptionPane.ERROR_MESSAGE);
+		}
 		return true;
 	}
 }
